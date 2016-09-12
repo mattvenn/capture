d7675022d27997ff52ae9ff657140861d07855a8
root <root@beaglebone.localdomain>
2016-09-12 20:39:10 +0000

    tested

--- capture.c
+++ capture.c
@@ -54,13 +54,11 @@ void sig_handler (int sig) {
 }
 
 void usage (char* arg0) {
-  fprintf(stderr, "\nUsage: %s [flags] pru0_code.bin pru1_code.bin\n",
+  fprintf(stderr, "\nUsage: %s [flags]\n",
           basename(arg0));
 
   fprintf(stderr, "\n"
-          "  -f freq\t gpio based clock frequency (default: 1000)\n"
-          "  -i [0-3]\t channel 0 input select\n"
-          "  -q [4-7]\t channel 1 input select\n"
+          "  -n max samples to take\n"
           "  -o output\t output filename (default: stdout)\n\n"
          );
   exit(EXIT_FAILURE);
@@ -69,10 +67,7 @@ void usage (char* arg0) {
 
 int main (int argc, char **argv) {
   int ch = -1;
-  double gpiofreq = 1000;
-double max_samp = 0;
-  int channel0_input = 0;
-  int channel1_input = 4;
+  double max_samp = 0;
   char* fname = "-";
   FILE* fout = stdout;
 
@@ -83,27 +78,10 @@ double max_samp = 0;
   }
 
   // Process command line flags
-  while (-1 != (ch = getopt(argc, argv, "n:f:i:q:o:"))) {
+  while (-1 != (ch = getopt(argc, argv, "n:o:"))) {
     switch (ch) {
     case 'n':
       max_samp = strtod(optarg, NULL);
-	break;
-    case 'f':
-      gpiofreq = strtod(optarg, NULL);
-      break;
-    case 'i':
-      channel0_input = strtol(optarg, NULL, 0);
-      if (channel0_input < 0 || channel0_input > 3) {
-        fprintf(stderr, "\n-i value must be between 0 and 3\n");
-        usage(argv[0]);
-      }
-      break;
-    case 'q':
-      channel1_input = strtol(optarg, NULL, 0);
-      if (channel1_input < 4 || channel1_input > 7) {
-        fprintf(stderr, "\n-q value must be between 4 and 7\n");
-        usage(argv[0]);
-      }
       break;
     case 'o':
       fname = optarg;
@@ -114,12 +92,10 @@ double max_samp = 0;
     }
   }
 
-  if (argc - optind != 2) {
+  if (argc - optind != 0) {
     usage(argv[0]);
     return EXIT_FAILURE;
   }
-
-	fprintf(stderr, "-- 1\n");
 
   argc -= optind;
   argv += optind;
@@ -130,24 +106,18 @@ double max_samp = 0;
       perror("unable to open output file");
     }
   }
-	fprintf(stderr, "-- 2\n");
 
   // Install signal handler to catch ctrl-C
   if (SIG_ERR == signal(SIGINT, sig_handler)) {
     perror("Warn: signal handler not installed %d\n");
   }
-	fprintf(stderr, "-- 3\n");
 
   // This segfaults if we're not root.
   prussdrv_init();
-	fprintf(stderr, "-- 3.1\n");
   if (0 != prussdrv_open(PRU_EVTOUT_0)) {
-	fprintf(stderr, "-- 3.2\n");
-    fprintf(stderr,
-            "prussdrv_open() failed. (Did you forget to run setup.sh?)\n");
+    fprintf(stderr, "prussdrv_open() failed. (Did you forget to run setup.sh?)\n");
     return EXIT_FAILURE;
   }
-	fprintf(stderr, "-- 4\n");
 
   tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
   prussdrv_pruintc_init(&pruss_intc_initdata);
@@ -163,7 +133,6 @@ double max_samp = 0;
   unsigned int shared_ddr_len = prussdrv_extmem_size();
   unsigned int physical_address = prussdrv_get_phys_addr((void*)shared_ddr);
 
-	fprintf(stderr, "-- 5\n");
   // Accessing the shared memory is slow, so later we'll efficiently copy it out
   // into this local buffer.
   uint32_t *local_buf = (uint32_t *) malloc(shared_ddr_len);
@@ -171,10 +140,6 @@ double max_samp = 0;
     fprintf(stderr, "Couldn't allocate memory.\n");
     return EXIT_FAILURE;
   }
-
-// wipe buffer
-//for(int i = 0; i < shared_ddr_len; i ++)
-//  shared_ddr[i] = 0;
 
   fprintf(stderr,
           "%uB of shared DDR available.\n Physical (PRU-side) address:%x\n",
@@ -192,45 +157,8 @@ double max_samp = 0;
   pparams->physical_addr = physical_address;
   pparams->ddr_len       = shared_ddr_len;
 
-  // Calculate the GPIO clock high and low cycle counts.
-  // Adding 0.5 and truncating is equivalent to rounding
-  int cycles = (PRU_CLK/gpiofreq + 0.5);
-  fprintf(stderr, "Actual GPIO clock speed is %.2fHz\n", PRU_CLK/((float)cycles));
-
-  if (cycles < 12) {
-    fprintf(stderr, "Requested frequency too high (max: 16666666)\n");
-    return EXIT_FAILURE;
-  }
-  pparams->high_cycles = cycles/2;
-  pparams->low_cycles  = cycles - pparams->high_cycles;
-
-  if (gpiofreq > 5e6) {
-    fprintf(stderr, "Sampling both channels faster than 5MSPS with prudaq_capture"
-            " is likely to cause buffer overruns due to limited DMA bandwidth."
-            " Consider using BeagleLogic's PRUDAQ support instead.\n");
-  }
-
-  // Decide the value that'll get written to PRU0's register r30
-  // See the docs for how bits in r30 correspond to the INPUT0A/
-  // INPUT0B/INPUT1A/INPUT1B control lines on the analog switches.
-  uint32_t pru0r30 = 0;
-  switch (channel0_input) {
-    case 0: break;
-    case 1: pru0r30 |= (1 << 1); break;
-    case 2: pru0r30 |= (1 << 2); break;
-    case 3: pru0r30 |= (1 << 1) | (1 << 2); break;
-  }
-  switch (channel1_input) {
-    case 4: break;
-    case 5: pru0r30 |= (1 << 3); break;
-    case 6: pru0r30 |= (1 << 5); break;
-    case 7: pru0r30 |= (1 << 3) | (1 << 5); break;
-  }
-  pparams->input_select = pru0r30;
-
   // Load the .bin files into PRU0 and PRU1
-  prussdrv_exec_program(0, argv[0]);
-  prussdrv_exec_program(1, argv[1]);
+  prussdrv_exec_program(1, "pru1.bin");
 
   uint32_t max_index = shared_ddr_len / sizeof(shared_ddr[0]);
   uint32_t read_index = 0;
@@ -279,10 +207,10 @@ double max_samp = 0;
 
       memcpy(local_buf, (void *) &(shared_ddr[read_index]), tail_bytes);
       bytes_read += tail_bytes;
-
       for (int i = 0; i < tail_words; i++) {
-        local_buf[i] &= 0x0fff0fff;
+		local_buf[i] &= 0x0fff0fff;
       }
+
 
       int head_bytes = write_index * sizeof(*shared_ddr);
       memcpy(&(local_buf[tail_words]), (void *) shared_ddr, head_bytes);
@@ -317,15 +245,12 @@ double max_samp = 0;
     usleep(100);
   }
 
-  // Wait for the PRU to let us know it's done
-  //prussdrv_pru_wait_event(PRU_EVTOUT_0);
   fprintf(stderr, "All done\n");
 
         uint32_t bytes_written = pparams->bytes_written;
 	uint32_t samples = bytes_written / 4;
         fprintf(stderr, "got %u samples\n", samples );
 
-  prussdrv_pru_disable(0);
   prussdrv_pru_disable(1);
   prussdrv_exit();
 
